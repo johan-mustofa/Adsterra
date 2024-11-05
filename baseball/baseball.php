55c4a231c24b0df46670c70d574ddc3bb7d408f9
Johan Mustofa <90010386+johan-mustofa@users.noreply.github.com>
2024-11-05 09:06:18 +0700

    Add files via upload

---
+++ baseball/baseball.php
@@ -0,0 +1,90 @@
+<?php include('../header.php');?>
+  <body data-spy="scroll" data-target=".onpage-navigation" data-offset="60">
+    <main>
+              <?php include('../menu.php');?>
+                    <div class="main"><section class="">
+  <div class="container">
+    <div class="row">
+        <div class="col-sm-4 col-md-2 col-md-2 sidebar">
+            <div class="widget">
+                <marquee><h5 class="font-alt"><?php echo $meta_title;?> Baseball</h5></marquee>
+            </div>
+        </div>
+        <div class="col-sm-8 col-md-10 col-md-10">
+          <div id="schedule" class="row">
+            <h4 class="font-alt mb-0"><i class="fa fa-angle-double-right"></i> Live Events</h4>
+            <hr class="divider-w mt-10 mb-20">
+              <div class="col-12">
+                <?php 
+   /*
+   | -------------------------------------------------------------------------------
+   | Author            : G-Silvers
+   | Template Name     : G-Silvers Sports Landing Page Special
+   | -------------------------------------------------------------------------------
+   */
+include("../app/grab.php");?>
+                <?php 
+                  $html = file_get_html('https://www.sofascore.com/api/v1/sport/baseball/events/live');
+    $data =  json_decode($html,true);
+    
+foreach($data['events'] as $elem)  {
+    $league = $elem['tournament']['name'];
+    $category = $elem['tournament']['category']['name'];
+    $sport = $elem['tournament']['category']['sport']['name'];
+    $usercount = $elem['tournament']['uniqueTournament']['userCount'];
+    $description = $elem['status']['description'];
+    $first_title = $elem['homeTeam']['name'];
+    $first_logo = 'https://www.sofascore.com/api/v1/team/'.$elem['homeTeam']['id'].'/image';
+    $homescore = $elem['homeScore']['current'];
+    $status = $elem['status']['type'];
+    $second_title = $elem['awayTeam']['name'];
+    $second_logo = 'https://www.sofascore.com/api/v1/team/'.$elem['awayTeam']['id'].'/image';
+    $awayscore = $elem['awayScore']['current'];
+    $slug = $elem['slug'];
+    $dt = $elem['startTimestamp'];
+    $id = $elem['id'];
+        $background_image = $url_webs . "/assets/gsilvers/images/baseball.jpg";
+    $date = new DateTime;
+    $date->setTimestamp($dt);
+    $dates = $date->format('D, j M Y g:i A | T | e');
+
+
+                ?>
+                                <div class="col-sm-12 col-md-6 col-lg-6 gsilvers">
+                  <a href="<?php echo $url_webs;?>baseball.php">
+                      <div class="post-thumbnail">
+                          <div class="gallery-item">
+                            <div class="gallery-image">
+                              <img src="<?php echo $background_image;?>" alt="<?php echo $sl;?>">
+                              <div class="gallery-logo-kiri">
+                                <img src="<?php echo $first_logo;?>" alt="<?php echo $first_title;?>">
+                              </div>
+                              <div class="gallery-logo-kanan">
+                                <img src="<?php echo $second_logo;?>" alt="<?php echo $second_title;?>">
+                              </div>
+                              <div class="gallery-caption">
+                                <div class="gallery-icon"><span class="fa fa-play-circle-o"></span></div>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                    <h4 class="menu-title font-alt"><img src="<?php echo $first_logo;?>"/> <?php echo $first_title;?></h4>                    
+                    <h4 class="menu-title font-alt"><img src="<?php echo $second_logo;?>"/> <?php echo $second_title;?></h4>                    
+                    <div class="menu-detail font-serif"><?php echo $league;?> | <?php echo $category;?> | <?php echo $description;?>/<?php echo $status;?> </div>
+                    <div class="menu-detail font-serif"><?php echo $dates;?> </div>
+                  </a>
+                </div>
+                <?php 
+                  }
+                  ?>
+                    <div class="Cell-sc-t6h3ns-0 iqwpGQ" style="width: 100%; height: 360px; flex-direction: column; padding: 16px;">
+                      <div style="color: rgb(128, 128, 128); margin: 0px 0px 4px; text-align: center;">
+                      </div>
+                    </div>
+                              </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</section>
+<?php include('../footer.php');?>
---
+++ menu.php
@@ -0,0 +1,58 @@
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            <a class="navbar-brand" href="<?php echo $url_webs;?>"><?php echo $logo;?></a>
+          </div>
+          <div class="collapse navbar-collapse" id="custom-collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Baseball</a>
+                <ul class="dropdown-menu">
+                  <li><a href="<?php echo $url_webs;?>baseball/baseball.php">Live</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Basketball</a>
+                <ul class="dropdown-menu">
+                  <li><a href="<?php echo $url_webs;?>basketball/basketball.php">Live</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">American Football</a>
+                <ul class="dropdown-menu">
+                  <li><a href="<?php echo $url_webs;?>american football/american football.php">Live</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Ice Hockey</a>
+                <ul class="dropdown-menu">
+                  <li><a href="<?php echo $url_webs;?>ice hockey/ice hockey.php">Live</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">football</a>
+                <ul class="dropdown-menu">
+                  <li><a href="<?php echo $url_webs;?>football/football.php">Live</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Tennis</a>
+                <ul class="dropdown-menu">
+                  <li><a href="<?php echo $url_webs;?>tennis/tennis.php">Live</a></li>
+                </ul>
+              </li>
+                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">More</a>
+                    <ul class="dropdown-menu"
+                        </li>
+                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Volleyball</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="<?php echo $url_webs;?>volleyball/volleyball.php">Live</a></li>
+                    </ul>
+                </li>
+                    <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Rugby</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="<?php echo $url_webs;?>rugby/rugby.php">Live</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                </li>
+              <li><a href="" style="font-weight:bold;" data-toggle="modal" data-target="#player-modal" data-backdrop="static" data-keyboard="false"><span class="fa fa-user-circle fa-lg"></span> Login</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
